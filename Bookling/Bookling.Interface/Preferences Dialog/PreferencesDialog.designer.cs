697d199e4d965c5601d33ecf0c681f04203d4285
George Burton <burtonageo@gmail.com>
2012-11-30 13:26:46 +0000

    Started implementation of paper texture on about window Started main design of PreferencesWindow Moved PreferencesDialog classes into the Bookling.Interface namespace

--- Bookling/Bookling.Interface/Preferences Dialog/PreferencesDialog.designer.cs
+++ Bookling/Bookling.Interface/Preferences Dialog/PreferencesDialog.designer.cs
@@ -1,17 +1,70 @@
+// WARNING
+//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoMac.Foundation;
 
-namespace Bookling
+namespace Bookling.Interface
 {
-	
-	// Should subclass MonoMac.AppKit.NSWindow
-	[MonoMac.Foundation.Register("PreferencesDialog")]
-	public partial class PreferencesDialog
+	[Register ("PreferencesDialogController")]
+	partial class PreferencesDialogController
 	{
+		[Outlet]
+		MonoMac.AppKit.NSSegmentedControl viewSelector { get; set; }
+
+		[Outlet]
+		MonoMac.AppKit.NSBox viewBox { get; set; }
+
+		[Outlet]
+		MonoMac.AppKit.NSButton acceptButton { get; set; }
+
+		[Outlet]
+		MonoMac.AppKit.NSButton resetButton { get; set; }
+
+		[Action ("ResetToDefaults:")]
+		partial void ResetToDefaults (MonoMac.Foundation.NSObject sender);
+
+		[Action ("Accept:")]
+		partial void Accept (MonoMac.Foundation.NSObject sender);
+
+		[Action ("Cancel:")]
+		partial void Cancel (MonoMac.Foundation.NSObject sender);
+
+		[Action ("SwitchView:")]
+		partial void SwitchView (MonoMac.Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (viewSelector != null) {
+				viewSelector.Dispose ();
+				viewSelector = null;
+			}
+
+			if (viewBox != null) {
+				viewBox.Dispose ();
+				viewBox = null;
+			}
+
+			if (acceptButton != null) {
+				acceptButton.Dispose ();
+				acceptButton = null;
+			}
+
+			if (resetButton != null) {
+				resetButton.Dispose ();
+				resetButton = null;
+			}
+		}
 	}
-	
-	// Should subclass MonoMac.AppKit.NSWindowController
-	[MonoMac.Foundation.Register("PreferencesDialogController")]
-	public partial class PreferencesDialogController
+
+	[Register ("PreferencesDialog")]
+	partial class PreferencesDialog
 	{
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
 	}
 }
-
--- Bookling/Bookling.Interface/Preferences Dialog/PreferencesDialogController.cs
+++ Bookling/Bookling.Interface/Preferences Dialog/PreferencesDialogController.cs
@@ -1,11 +1,35 @@
-
+//
+// PreferencesDialogController.cs
+//
+// Author:
+//       georgeburton <burtonageo@gmail.com>
+//
+// Copyright (c) 2012 georgeburton
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using MonoMac.Foundation;
 using MonoMac.AppKit;
 
-namespace Bookling
+namespace Bookling.Interface
 {
 	public partial class PreferencesDialogController : MonoMac.AppKit.NSWindowController
 	{
@@ -33,6 +57,9 @@ namespace Bookling
 		// Shared initialization code
 		void Initialize ()
 		{
+			//resetButton = new NSButton ();
+			resetButton.Enabled = false;
+			//acceptButton.Enabled = false;
 		}
 		
 		#endregion
@@ -44,6 +71,26 @@ namespace Bookling
 			}
 		}
 
+		partial void ResetToDefaults (MonoMac.Foundation.NSObject sender)
+		{
+
+		}
+		
+		partial void Accept (MonoMac.Foundation.NSObject sender)
+		{
+
+		}
+		
+		partial void Cancel (MonoMac.Foundation.NSObject sender)
+		{
+			Window.OrderOut (sender);
+			NSApplication.SharedApplication.StopModal ();
+		}
+		
+		partial void SwitchView (MonoMac.Foundation.NSObject sender)
+		{
+
+		}
 	}
 }
 
