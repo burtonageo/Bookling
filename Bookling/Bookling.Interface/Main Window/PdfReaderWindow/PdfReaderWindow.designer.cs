e908fe7d8df578f1200bc628bf2ae59955859559
George Burton <burtonageo@gmail.com>
2013-02-24 14:46:54 +0000

    Added PdfReaderWindow classes

---
+++ Bookling/Bookling.Interface/Main Window/PdfReaderWindow/PdfReaderWindow.designer.cs
@@ -0,0 +1,17 @@
+
+namespace Bookling.Interface
+{
+	
+	// Should subclass MonoMac.AppKit.NSWindow
+	[MonoMac.Foundation.Register("PdfReaderWindow")]
+	public partial class PdfReaderWindow
+	{
+	}
+	
+	// Should subclass MonoMac.AppKit.NSWindowController
+	[MonoMac.Foundation.Register("PdfReaderWindowController")]
+	public partial class PdfReaderWindowController
+	{
+	}
+}
+
